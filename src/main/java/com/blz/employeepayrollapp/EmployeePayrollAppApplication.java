ca962487ee1f5c3e1d7418adc76b82f2683e17c4
Sampada-VS <samssv1@gmail.com>
2020-12-22 03:15:26 +0530

    Added: slf4j for logging and logging levels,patterns are handled in application.properties

--- src/main/java/com/blz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/blz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,11 +2,16 @@ package com.blz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ApplicationContext context=SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee payroll app started in {} environment..",context.getEnvironment().getProperty("environment"));
 	}
 }

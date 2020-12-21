0557fbe8097745be893d33ad473fdb105cb98b72
Sampada-VS <samssv1@gmail.com>
2020-12-22 02:48:33 +0530

    Added: lombok library to generate getters and setters for response dto using @Data

--- src/main/java/com/blz/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/blz/employeepayrollapp/dto/ResponseDTO.java
@@ -1,6 +1,8 @@
 package com.blz.employeepayrollapp.dto;
 
-public class ResponseDTO {
+import lombok.Data;
+
+public @Data class ResponseDTO {
 
 	private String message;
 	private Object data;
@@ -10,19 +12,4 @@ public class ResponseDTO {
 		this.data = data;
 	}
 
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
 }

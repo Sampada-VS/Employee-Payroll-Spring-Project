a1eae2f1e6fda9aa93b21f5b088f3e4c3755c960
Sampada-VS <samssv1@gmail.com>
2020-12-22 20:29:35 +0530

    Added: user friendly error response in case employee id not found

---
+++ src/main/java/com/blz/employeepayrollapp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.blz.employeepayrollapp.exceptions;
+
+public class EmployeePayrollException extends RuntimeException {
+    public EmployeePayrollException(String message){
+        super(message);
+    }
+}

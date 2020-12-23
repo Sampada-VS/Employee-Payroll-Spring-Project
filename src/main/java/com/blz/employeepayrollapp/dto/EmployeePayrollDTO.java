48378bb4ddd44610e3e49157fd07353871e16086
Sampada-VS <samssv1@gmail.com>
2020-12-23 15:50:45 +0530

    Added: validations for new fields and exception handler for invalid date format

--- src/main/java/com/blz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/blz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -18,15 +21,21 @@ public @ToString class EmployeePayrollDTO {
 	@Max(value=500000,message="Salary should be less than 500000")
 	public long salary;
 
+	@NotBlank(message = "Profile Image cannot be empty")
 	public String profileImg;
 
+	@Pattern(regexp="Male|Female", message="Gender should be Male or Female")
 	public String gender;
 
+	@NotNull(message = "Department should not be empty")
 	public List<String> department;
 
+	@NotNull(message = "Department should not be empty")
+	@PastOrPresent(message = "Start date should be past or today's date")
 	@JsonFormat(pattern = "dd MMM yyyy")
 	public LocalDate startDate;
 
+	@NotBlank(message = "Notes cannot be empty")
 	public String notes;
 
 }
--- src/main/java/com/blz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/blz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -7,27 +7,38 @@ import com.blz.employeepayrollapp.dto.ResponseDTO;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler  {
-    
+    private static final String message="Exception while processing REST request";
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
         List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
         List<String> errMsg=errorList.stream()
                                     .map(objErr -> objErr.getDefaultMessage())
                                     .collect(Collectors.toList());
-        ResponseDTO responseDTO=new ResponseDTO("Exception while processing REST request", errMsg);
+        ResponseDTO responseDTO=new ResponseDTO(message, errMsg);
         return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmployeePayrollException.class)
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception){
-        ResponseDTO responseDTO=new ResponseDTO("Exception while processing REST request", exception.getMessage());
+        ResponseDTO responseDTO=new ResponseDTO(message, exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("Invalid Date format", exception);
+        ResponseDTO responseDTO=new ResponseDTO(message, "Date should be in dd MMM yyyy format");
         return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
     }
 }
